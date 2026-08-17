3b65ed394c2e0095ba10be5dc63f751f9d689048
Ruzayna Shifa <ruzayna.shifa@gmail.com>
2026-08-17 09:29:14 +0600

    Add files via upload

---
+++ Layer 1 & 2/User.h
@@ -0,0 +1,57 @@
+#ifndef USER_H
+#define USER_H
+
+#include <iostream>
+
+class User {
+public:
+    User();
+
+    User(int id, const char* name);
+    User(const User& other);
+    User& operator=(const User& other);
+    ~User();
+
+    int getId() const;
+    const char* getName() const;
+
+
+    bool operator==(const User& other) const;
+    bool operator<(const User& other) const;
+    friend std::ostream& operator<<(std::ostream& out, const User& user);
+
+private:
+    int id;
+    char* name;
+
+    void copyFrom(const User& other);
+};
+
+#endif
+#define USER_H
+
+#include <iostream>
+
+
+class User {
+public:
+    User();
+    User(int id, const char* name);
+    User(const User& other);
+    User& operator=(const User& other);
+    ~User();
+
+    int getId() const;
+    const char* getName() const;
+
+    bool operator==(const User& other) const;
+    bool operator<(const User& other) const;
+    friend std::ostream& operator<<(std::ostream& out, const User& user);
+
+private:
+    int id;
+    char* name;
+    void copyFrom(const User& other);
+};
+
+#endif
